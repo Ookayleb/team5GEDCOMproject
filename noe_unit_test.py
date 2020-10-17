b19ebbb0d74cc7df6a7d73e72a6281c7a8fbadac
noedurocher <noedurocher@gmail.com>
2020-10-17 19:47:01 -0400

    Add new unit test and new data

--- noe_unit_test.py
+++ noe_unit_test.py
@@ -1,65 +1,95 @@
 import unittest
-from script import generateInitialData, reset, get_age_difference,get_deceased_records, get_parents_not_too_old, getChildren_and_age, lookup
+import script
+from script import get_deceased_records, generateInitialData, reset, get_parents_not_too_old, get_age_difference, replace_id_with_children_data, get_individual_age
 
-gedcomeStructuredData = generateInitialData("family_project.ged")
-indiList = 	gedcomeStructuredData['indiList']
-arr = []
-for record in indiList:
-	arr.append(record['ID'])
-reset()
+class TestGetDeceasedRecords(unittest.TestCase):
+	def test_get_deceased_records_1(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		indiList = gedcomStructuredData['indiList']
+		reset()
+		result = get_deceased_records(indiList)[0]
+		self.assertEqual(result, 1)
+
+	def test_get_deceased_records_2(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		indiList = gedcomStructuredData['indiList']
+		reset()
+		result = get_deceased_records(indiList)[1]
+		self.assertTrue(result)
 
 
-famiList = gedcomeStructuredData['famList']
-reset()
 
-class noe_unit_test(unittest.TestCase):
-	def test_getChildren_and_age(self):
-		self.assertTrue(getChildren_and_age(arr[0]))
-		self.assertTrue(getChildren_and_age(arr[1]))
-		self.assertTrue(getChildren_and_age(arr[2]))
-		
+class TestGetParentsNotTooOld(unittest.TestCase):
+	def test_get_parents_not_too_old_1(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		famList	= gedcomStructuredData['famList']
+		reset()
+		result = get_parents_not_too_old(famList)[0]
+		self.assertEqual(result, 1)
+
+	def test_get_parents_not_too_old_2(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		famList	= gedcomStructuredData['famList']
+		reset()
+		result = get_parents_not_too_old(famList)[1]
+		self.assertTrue(result)
+
 	def test_get_age_difference(self):
-		self.assertEqual(get_age_difference(75, 25), 50)
-		self.assertNotEqual(get_age_difference(44, 20), 21)
+		self.assertEqual(get_age_difference(120, 70), 50)
 
-	def test_get_deceased_records(self):
-		self.assertTrue(get_deceased_records(indiList))
-	
-# gedcomeStructuredData2 = generateInitialData("family_project.ged")
-# indiList2 = 	gedcomeStructuredData2['indiList'] 
-# reset()
+	def test_replace_id_with_children_data(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		famList	= gedcomStructuredData['famList']
+		reset()
 
-# gedcomeStructuredData3 = generateInitialData("family_project.ged")
-# indiList3 = 	gedcomeStructuredData3['indiList']
-# reset()
-
-# gedcomeStructuredData4 = generateInitialData("family_project.ged")
-# indiList4 = 	gedcomeStructuredData4['indiList']
-# reset()
-
-# gedcomeStructuredData5 = generateInitialData("family_project.ged")
-# indiList5 = 	gedcomeStructuredData5['indiList']
+		for i in range(len(famList)):
+			result = replace_id_with_children_data([famList[i]['Children']])
+		self.assertTrue(result)
 
 
-# class GedcomTest(unittest.TestCase):
 
-# 	def test_print_age_qualificationh(self):
-# 		self.assertTrue(print_age_qualification(indiList1),
-# 			[['Bobby Smith ', '1890-9-26', 'No'], ['Kate Smith ', '2012-7-05', 'Yes'], ['John Smith ', '2016-10-31', 'Yes']])
-	
-#     def test_print_age_qualificationh(self):
-# 		self.assertTrue(print_age_qualification(indiList),[['Bobby Smith ', '1890-9-26', 'No'], ['Kate Smith ', '2012-7-05', 'Yes'], ['John Smith ', '2016-10-31', 'Yes']])
+class TestGetIndividualAge(unittest.TestCase):
+	def test_get_individual_ages(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		indiList = gedcomStructuredData['indiList']
+		reset()
+		result = get_individual_age(indiList)
+		self.assertTrue(result)
 
-#     def test_print_age_qualificationh(self):
-# 		self.assertTrue(print_age_qualification(indiList),
-# 			[['Bobby Smith ', '1890-9-26', 'No'], ['Kate Smith ', '2012-7-05', 'Yes'], ['John Smith ', '2016-10-31', 'Yes']])
 
-#     def test_print_age_qualificationh(self):
-# 		self.assertTrue(print_age_qualification(indiList),
-# 			[['Bobby Smith ', '1890-9-26', 'No'], ['Kate Smith ', '2012-7-05', 'Yes'], ['John Smith ', '2016-10-31', 'Yes']])
 
-    # def test_get_deceased_records(self):
-	# 	self.assertTrue(get_deceased_records(indiList))
+class TestGetLivingMarried(unittest.TestCase):
+	def test_find_family_ids(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		famList	= gedcomStructuredData['famList']
+		reset()
+		result = script.find_family_ids(famList)
+		self.assertTrue(result)
+
+	def test_get_married_list(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		famList	= gedcomStructuredData['famList']
+		reset()
+		result = script.get_married_list(famList)
+		self.assertTrue(result)
+
+	def test_get_living_married(self):
+		reset()
+		gedcomStructuredData = generateInitialData("gedFiles/Choy_family.ged")
+		indiList = gedcomStructuredData['indiList']
+		reset()
+		famList	= gedcomStructuredData['famList']
+		reset()
+		result = script.get_living_married(indiList, famList)
+		self.assertTrue(result)
 
 
 if __name__ == '__main__':
